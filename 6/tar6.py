56565a02d4392f06c6f2e557124dec23d9cb85b9
GuedaliaMahonAccount <gsebbah@g.jct.ac.il>
2025-06-23 14:53:15 +0300

    finish

--- 6/tar6.py
+++ 6/tar6.py
@@ -1,50 +1,65 @@
 #
-#  Targil 2
+#  Targil 6
 #
 ##Noam Hadad 322766064
 ##Guedalia Sebbah
 
 
-
 #
-#f1
+# question 1
 def f1():
 
-#
-#f2
 
+#
+# question 2
+def f2():
+
+
+#
+# question 3
+def f3():
+
+
+#
+# question 4
+def f4():
+
+
+#
+# question 5
+def f5():
 
 
 if __name__ == '__main__':
-    # map menu choices to your functions
     functions = {
         '1': f1,
         '2': f2,
         '3': f3,
         '4': f4,
         '5': f5,
-        '6': f6,
+    }
+    descriptions = {
+        '1': 'Pentagonal numbers range and display',
+        '2': 'Sum of digits',
+        '3': 'Palindrome check',
+        '4': 'Series-sum calculation',
+        '5': 'Merge three dictionaries',
     }
 
-    print("Welcome to Targil 1")
-    print("---------------------------------------")
+    print("Welcome to Targil 2")
+    print("-------------------")
 
     while True:
-        # print menu
-        print("\nyour choices:")
+        print("\nYour choices:")
         print("0 : exit")
-        for num in sorted(functions):
-            print(f"{num} : function {num}")
-
-        # get choice
-        choice = input("please enter your choice ")
-
-        # dispatch
+        for key in sorted(functions):
+            print(f"{key} : {descriptions[key]}")
+        choice = input("Please enter your choice: ")
         if choice == '0':
             print("Bye!")
             break
         elif choice in functions:
-            print()           # extra line before function runs
+            print()
             functions[choice]()
         else:
             print("Invalid choice, try again.")
